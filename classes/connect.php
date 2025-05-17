65495e3c93ee17daa408d7611bb4e2e873816f26
chvrif001 <111813903+chvrif001@users.noreply.github.com>
2025-05-17 13:13:32 +0200

    Update connect.php

--- classes/connect.php
+++ classes/connect.php
@@ -2,10 +2,10 @@
 
 class Database
 {
-	private $host = "sql202.infinityfree.com"; //Change here if you want to connect to a different host//
-	private $username = "if0_38862256" ; 
-	private $password = "VLL72PR3Ipb4O";
-	private $db = "if0_38862256_cskpredatordetector_db";
+	private $host = "dpg-d0evn2c9c44c738hdq4g-a"; //Change here if you want to connect to a different host//
+	private $username = "cskpredatordetetor_db_user" ; 
+	private $password = "zk90UPRI8WZHEC1TVdNFtNm0aPOX12aP";
+	private $db = "cskpredatordetetor_db";
 
 
 	function connect()
