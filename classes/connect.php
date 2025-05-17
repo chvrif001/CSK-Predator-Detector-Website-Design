f33aa3b9f49960979332f70066779c1e5322e4e5
chvrif001 <111813903+chvrif001@users.noreply.github.com>
2025-05-17 14:04:07 +0200

    Update connect.php

--- classes/connect.php
+++ classes/connect.php
@@ -2,52 +2,46 @@
 
 class Database
 {
-	private $host = "dpg-d0evn2c9c44c738hdq4g-a.oregon-postgres.render.com"; //Change here if you want to connect to a different host//
-	private $username = "cskpredatordetetor_db_user" ; 
+	private $host = "dpg-d0evn2c9c44c738hdq4g-a.oregon-postgres.render.com";
+	private $username = "cskpredatordetetor_db_user";
 	private $password = "zk90UPRI8WZHEC1TVdNFtNm0aPOX12aP";
 	private $db = "cskpredatordetetor_db";
 
-
 	function connect()
 	{
-		$connection = mysqli_connect($this->host, $this->username, $this->password, $this->db);
+		$conn_string = "host=$this->host dbname=$this->db user=$this->username password=$this->password";
+		$connection = pg_connect($conn_string);
+
+		if (!$connection) {
+			die("PostgreSQL connection failed.");
+		}
+
 		return $connection;
 	}
-
 
 	function read($query)
 	{
 		$conn = $this->connect();
-		$result = mysqli_query($conn,$query);
+		$result = pg_query($conn, $query);
 
-		if (!$result)
-		{
+		if (!$result) {
 			return false;
 		}
-		else
-		{
-			$data = false;
-			while( $row = mysqli_fetch_array($result))
-			{
-				$data[] = $row;
-				
-			}
-			return $data;
+
+		$data = [];
+		while ($row = pg_fetch_assoc($result)) {
+			$data[] = $row;
 		}
-
+		return $data;
 	}
-
 
 	function save($query)
 	{
 		$conn = $this->connect();
-		$result = mysqli_query($conn,$query);
-
-		if (!$result)
-		{return false;}
-		else{return true;}
+		$result = pg_query($conn, $query);
+		return $result !== false;
 	}
 }
-
+?>
 
 
